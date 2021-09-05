501b9a7dd070c711f6b93f3a5c1ff7debccd8aa3
vaishnav319 <msvsurya1@gmail.com>
2021-09-05 13:07:59 +0530

    initial commit

---
+++ src/HOC/Default.HOC.js
@@ -0,0 +1,27 @@
+//Transforms component into another component
+//Adding additional functionalities to the existing components.
+
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+//Layouts
+import DefaultLayout from '../layouts/Default.Layout';
+
+const DefaultHOC = ({ component: Component, ...rest }) => {
+  //component
+  //props -> path exact
+  return (
+    <>
+      <Route
+        {...rest}
+        component={(props) => (
+          <DefaultLayout>
+            <Component {...props} />
+          </DefaultLayout>
+        )}
+      />
+    </>
+  );
+};
+
+export default DefaultHOC;
