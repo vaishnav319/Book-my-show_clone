33653eceb1966999d2c0f4342fef6d43342ea9b1
vaishnav319 <msvsurya1@gmail.com>
2021-09-10 23:39:46 +0530

    Added Movie page

--- src/pages/Movie.page.js
+++ src/pages/Movie.page.js
@@ -1,11 +1,9 @@
 import React from 'react';
-
+import MovieHero from '../components/MovieHero/MovieHero.component';
 const Moviepage = () => {
   return (
     <>
-      <div>
-        <img src='https://in.bmscdn.com/iedb/movies/images/mobile/listing/medium/shang-chi-and-the-legend-of-the-ten-rings-et00122566-24-08-2021-02-01-36.jpg' />
-      </div>
+      <MovieHero />
     </>
   );
 };
